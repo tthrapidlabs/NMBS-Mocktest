7f164426561663ba4e15a4fedadf552b58f5b220
Akhila Bojanapalli <2184274@tcs.com>
2024-06-10 15:43:21 +0530

    changed'

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask,request,render_template,redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    """
+        return hello World
+    """
+    return "Hello World! This is Flask."
